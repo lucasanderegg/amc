a75477140ddfe3d522cf02118849ee417aed6ed2
Lucas Anderegg <lucasanderegg@Lucass-MacBook.local>
2018-11-30 15:14:07 +0100

    del

--- src/app/can-deactivate-contacts-editor-guard.ts
+++
@@ -1,5 +0,0 @@
-import {ContactsEditorComponent} from './contacts-editor/contacts-editor.component'
-import {CanDeactivate} from '@angular/router'
-
-export class CanDeactivateContactsEditorGuard {
-}
